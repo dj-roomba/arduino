c31a362253cfdd43e5afacbe30c8bd7bd29c6cd2
Brian Cruz <cruz.s.brian@gmail.com>
2018-08-09 10:59:35 -0400

    Times and calculates the cpm on each blip, no filtering

--- cpm/cpm.ino
+++ cpm/cpm.ino
@@ -1,7 +1,13 @@
+#include <elapsedMillis.h>
+
 #define radSignal 2
 #define led 13
 
 volatile bool blip = false;
+
+float raw_cpm = 0;
+int cpm = 0;
+elapsedMillis period;
 
 void setup() {
   pinMode(radSignal, INPUT);
@@ -11,17 +17,26 @@ void setup() {
   attachInterrupt(0, onBlip, RISING);
 
   digitalWrite(led, LOW);
+
+  Serial.begin(9600);
+
+  Serial.println("test");
+
+  period = 0;
 }
 
 void loop() {
-  if (blip) {
-    digitalWrite(led, HIGH);
-    delay(50);
-    digitalWrite(led, LOW);
+  if (blip && period != 0) {
+    raw_cpm = 60. * 1000. / (float)period; // period in ms, cpm in 1/min
+    String cpmStr = String(raw_cpm);
+    Serial.println(cpmStr);
+
+    period = 0;
+
     blip = false;
   }
 
-  delay(10);
+  delay(1);
 }
 
 void onBlip() {
