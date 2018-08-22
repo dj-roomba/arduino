6d8b9358d468c4c28c500e6e36d73cf6652726ab
Brian Cruz <cruz.s.brian@gmail.com>
2018-08-22 10:23:06 -0400

    Simply prints the count for every 0.1s

--- cpm/cpm.ino
+++ cpm/cpm.ino
@@ -1,65 +1,24 @@
-#include <elapsedMillis.h>
-
-#define radSignal 2
-
-#define alpha 0.3
-
-volatile bool blip = false;
-
 volatile int blipCt = 0;
 
-float raw_cpm = 0;
-float cpm_sample;
-float cpm;
-elapsedMillis period;
+float cpm = 0;
 
 void setup() {
-  pinMode(radSignal, INPUT);
+  pinMode(2, INPUT);
 
   // pin 2 corresponds to interrupt vector 0
   attachInterrupt(0, onBlip, RISING);
 
   Serial.begin(9600);
-
-  Serial.println("test");
-
-  period = 0;
 }
 
 void loop() {
-  /*if (blip && period != 0 && period != 0.1) {
-    raw_cpm = 60. * 1000. / (float)period; // period in ms, cpm in 1/min
-
-    // low pass filter, lower alpha means less sensitive to changes
-    cpm += alpha * (raw_cpm - cpm);
-
-    // print to serial monitor
-    String cpmStr = String(cpm);
-    Serial.println(cpmStr);
-
-    period = 0;
-
-    blip = false;
-  }
-
-  delay(1);*/
-
-  delay(500);
-
-  cpm_sample = blipCt * 120;  // counts in half a second to counts in one minute
+  delay(100);
   
-  // low pass filter, lower alpha means less sensitive to changes
-  cpm += alpha * (cpm_sample - cpm);
-  
-  // print to serial monitor
-  String cpmStr = String(cpm);
-  Serial.println(cpmStr);
-
+  Serial.println(blipCt);
   blipCt = 0;
 }
 
 void onBlip() {
-  blip = true;
   blipCt++;
 }
 
